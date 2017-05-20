f442eacb36dafe08fde63407805badd3fb064683
vidura185 <viduraamarasinghe@outlook.com>
2017-05-20 07:19:59 +0530

    Added the initial code to send HTTP request and parse JSON file. Not complete

---
+++ Assets/Scripts/Health Statistics/UserActivity.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class UserActivity : MonoBehaviour {
+    ActivityRecord userActivity;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(GetHealthStatistics());
+	}
+	
+    IEnumerator GetHealthStatistics ()
+    {
+        //sending a GET request
+        string requestURL = "http://localhost/JSONtest.JSON";
+        UnityWebRequest request = UnityWebRequest.Get(requestURL);
+        yield return request.Send();
+
+        if (request.error == null)
+        {
+            ParseJSON(request.downloadHandler.text);
+        } else
+        {
+            Debug.Log("Retrieving updated health statistics failed!");
+        }
+    }
+
+    private void ParseJSON(string jsonContent)
+    {
+        userActivity = JsonMapper.ToObject<ActivityRecord>(jsonContent);
+    }
+}
+public class ActivityRecord
+{
+    string userId;
+    int steps;
+}
